f5f04a25ba7310f7eb6eeac2303e2f4a3404164d
AlonSchwartz <karamon@gmail.com>
2024-04-21 12:53:51 +0300

    deleted unnecessary routes

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,32 +1,14 @@
 import express from 'express';
-import { loginUser, logoutUser, checkAuth } from '../controllers/userController.js';
-import { addToFavorites, getAllRecipes, createHashValue, getRecipe } from '../controllers/recipeController.js';
+import { checkAuth } from '../controllers/userController.js';
+import { addToFavorites, getRecipe } from '../controllers/recipeController.js';
 import { createRecipe } from '../controllers/recipeController.js'
 
 const router = express.Router();
 
-//router.get("/urlCheck", hashId)
-
 router.post("/", createRecipe)
-
-//router.post("/login", loginUser)
-
-//router.post("/test", checkAuth)
-
-//router.post("/check-tokens", checkAuth, checkTokens)
 
 router.post("/save", checkAuth, addToFavorites)
 
-//router.get("/recipes", checkAuth, getAllRecipes)
-
-//router.post("/share", shareRecipe)
-
 router.get("/:id", getRecipe)
-
-//router.delete("/logout", logoutUser)
-
-
-
-
 
 export { router };
