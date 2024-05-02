9e8568c2cf25f93ad4f4cc3e4340d19d8ee36566
AlonSchwartz <karamon@gmail.com>
2024-05-02 14:18:13 +0300

    small refactoring to user controller, added auth constants

---
+++ utils/authConstants.js
@@ -0,0 +1,12 @@
+export const AUTH_MESSAGES = {
+    LOGIN_FAILED_MISMATCH: 'email/password does not match',
+    REGISTRATION_FAILED_EMAIL_IN_USE: 'Email address already in use.',
+    FAILED_TO_GENERATE_TOKENS: 'Failed to generate tokens',
+    LOGIN_SUCCESS: 'User logged in successfully',
+    REGISTRATION_SUCCESS: 'User created successfully',
+};
+
+export const AUTH_TYPE = {
+    LOGIN: 'login',
+    REGISTRATION: 'registration',
+};
